e010addf2083578114c241b45b132148fa6c5b08
Vasiliy9000 <60994491+Vasiliy9000@users.noreply.github.com>
2025-04-23 13:21:59 +0300

    Create options.php

---
+++ modules/cargonomica.main/lang/ru/options.php
@@ -0,0 +1,11 @@
+
+<?php
+$MESS['CRON_TAB_NAME'] = 'CRON';
+$MESS['CRON_TAB_TITLE'] = 'Настройка параметров CRON';
+
+$MESS['CRON_TAB_WON_DEALS_REPORT_TITLE'] = 'Отчёт о завершённых сделках';
+$MESS['CRON_TAB_REPORT_SEND_DAILY'] = 'Отправлять ежедневный отчёт';
+$MESS['CRON_TAB_REPORT_EMAIL'] = 'Email-адрес для отправки отчёта (можно указать несколько адресов через запятую)';
+$MESS['CRON_TAB_LAST_REPORT_TIMESTAMP'] = 'Дата и время последней отправки отчёта';
+$MESS['CRON_TAB_LAST_REPORT_DEALS_COUNT'] = 'Количество сделок, отправленных в последнем отчёте';
+$MESS['CRON_SAVE_SUCCESS'] = 'Настройки успешно сохранены';
